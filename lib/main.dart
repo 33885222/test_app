97223c5d3ae88a4701c4a8eb529aa25374e1f078
33885222 <33885222@mail.ru>
2024-06-30 09:20:11 +0300

    123231

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,86 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:wireguard_flutter/wireguard_flutter.dart';
+import 'package:flutter_vpn/flutter_vpn.dart';
+import 'package:flutter_vpn/state.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('WireGuard Example App'),
-        ),
-        body: const MyApp(),
-      ),
-    ),
-  );
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final wireguard = WireGuardFlutter.instance;
+  final _addressController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
-  late String name;
+  var state = FlutterVpnState.disconnected;
+  CharonErrorState? charonState = CharonErrorState.NO_ERROR;
 
   @override
   void initState() {
+    FlutterVpn.prepare();
+    FlutterVpn.onStateChanged.listen((s) => setState(() => state = s));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints.expand(),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: initialize,
-            style: ButtonStyle(
-                minimumSize:
-                    MaterialStateProperty.all<Size>(const Size(100, 50)),
-                padding: MaterialStateProperty.all(
-                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.blueAccent),
-                overlayColor: MaterialStateProperty.all<Color>(
-                    Colors.white.withOpacity(0.1))),
-            child: const Text(
-              'initialize',
-              style: TextStyle(color: Colors.white),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter VPN'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(12),
+          children: <Widget>[
+            Text('Current State: $state'),
+            Text('Current Charon State: $charonState'),
+            TextFormField(
+              controller: _addressController,
+              decoration: const InputDecoration(icon: Icon(Icons.map_outlined)),
             ),
-          ),
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: startVpn,
-            style: ButtonStyle(
-                minimumSize:
-                    MaterialStateProperty.all<Size>(const Size(100, 50)),
-                padding: MaterialStateProperty.all(
-                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.blueAccent),
-                overlayColor: MaterialStateProperty.all<Color>(
-                    Colors.white.withOpacity(0.1))),
-            child: const Text(
-              'Connect',
-              style: TextStyle(color: Colors.white),
+            TextFormField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person_outline),
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: disconnect,
-            style: ButtonStyle(
-                minimumSize:
-                    MaterialStateProperty.all<Size>(const Size(100, 50)),
-                padding: MaterialStateProperty.all(
-                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.blueAccent),
-                overlayColor: MaterialStateProperty.all<Color>(
-                    Colors.white.withOpacity(0.1))),
-            child: const Text(
-              'Disconnect',
-              style: TextStyle(color: Colors.white),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(icon: Icon(Icons.lock_outline)),
             ),
-          ),
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: getStatus,
-            style: ButtonStyle(
-                minimumSize:
-                    MaterialStateProperty.all<Size>(const Size(100, 50)),
-                padding: MaterialStateProperty.all(
-                    const EdgeInsets.fromLTRB(20, 15, 20, 15)),
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.blueAccent),
-                overlayColor: MaterialStateProperty.all<Color>(
-                    Colors.white.withOpacity(0.1))),
-            child: const Text(
-              'Get status',
-              style: TextStyle(color: Colors.white),
+            ElevatedButton(
+              child: const Text('Connect'),
+              onPressed: () => FlutterVpn.connectIkev2EAP(
+                server: _addressController.text,
+                username: _usernameController.text,
+                password: _passwordController.text,
+              ),
             ),
-          ),
-        ],
+            ElevatedButton(
+              child: const Text('Disconnect'),
+              onPressed: () => FlutterVpn.disconnect(),
+            ),
+            ElevatedButton(
+              child: const Text('Update State'),
+              onPressed: () async {
+                var newState = await FlutterVpn.currentState;
+                setState(() => state = newState);
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Update Charon State'),
+              onPressed: () async {
+                var newState = await FlutterVpn.charonErrorState;
+                setState(() => charonState = newState);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
